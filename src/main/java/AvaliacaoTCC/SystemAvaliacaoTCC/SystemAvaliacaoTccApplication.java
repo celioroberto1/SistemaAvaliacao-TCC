9c1d01fa22415c412e492c43c966f48fa12a0990
CelioAndrade <celio.roberto@academico.ifpb.edu.br>
2025-10-29 17:54:05 -0300

    primeiro commit

---
+++ src/main/java/AvaliacaoTCC/SystemAvaliacaoTCC/SystemAvaliacaoTccApplication.java
@@ -0,0 +1,13 @@
+package AvaliacaoTCC.SystemAvaliacaoTCC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemAvaliacaoTccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemAvaliacaoTccApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AvaliacaoTCC/SystemAvaliacaoTCC/SystemAvaliacaoTccApplicationTests.java
@@ -0,0 +1,13 @@
+package AvaliacaoTCC.SystemAvaliacaoTCC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemAvaliacaoTccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
